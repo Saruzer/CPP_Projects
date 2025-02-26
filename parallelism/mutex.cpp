ac749d32d2b009af1de954a03cf8234170634288
Saruzer Saruzer <mrlexa130@gmail.com>
2025-02-26 11:32:30 +0200

    Added work with mutex

--- parallelism/mutex.cpp
+++ parallelism/mutex.cpp
@@ -53,6 +53,7 @@ void PrintNumbers3(int count)
 	if(count > 0)
 	{
 		PrintNumbers3(count - 1);
+		std::cout << count << std::endl;
 	}
 	recursiveMutex.unlock();
 }
@@ -65,10 +66,72 @@ void RecursiveMutexExample()
 	thread2.join();
 }
 #pragma endregion
+#pragma region std::timed_mutex
+
+std::timed_mutex timedMutex;
+
+void UseResource(int threadindex)
+{
+	auto timeOut = std::chrono::seconds(2);
+	bool isLockedAcquired = false;
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	while(!isLockedAcquired)
+	{
+		isLockedAcquired = timedMutex.try_lock_until(startTime + timeOut);
+		if(isLockedAcquired)
+		{
+			std::cout << "Resource used by thread #" << threadindex << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(5));
+			timedMutex.unlock();
+		}
+		else
+		{
+			std::cout << "Thread # " << threadindex << " is waiting..." << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+	}
+}
+void TimedMutexExample()
+{
+	std::thread thread1(UseResource, 1);
+	std::thread thread2(UseResource, 2);
+	std::thread thread3(UseResource, 3);
+	std::thread thread4(UseResource, 4);
+
+	thread1.join();
+	thread2.join();
+	thread3.join();
+	thread4.join();
+}
+#pragma endregion
+#pragma region std::locked_guard
+std::mutex mutex2;
+
+void PrintNumbers5(int count)
+{
+	std::lock_guard<std::mutex> guard(mutex2);
+
+	for (int number = 0; number <= count; number++)
+	{
+		std::cout << number << std::endl;
+	}
+}
+
+void MutexExample()
+{
+	std::thread thread1(PrintNumbers5, 50);
+	std::thread thread2(PrintNumbers5, 50);
+
+	thread1.join();
+	thread2.join();
+}
+
+#pragma endregion
 
 
 int main()
 {
-	RecursiveMutexExample();
+	TimedMutexExample();
 	return 0;
 }
